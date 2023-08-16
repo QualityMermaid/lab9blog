509575c547d31ff4c8b67cf033fa87f2ec6491f8
JessicaBane <jessicambane@gmail.com>
2023-08-16 14:55:38 +0100

    skelenton

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,15 @@
+import Link from "next/link";
 
 export default function Header() {
   return (
-    <header>Header</header>
+    <header>
+        <h1>MerTails</h1>
+        <nav>
+            <ul>
+                <li><Link href="/">Home</Link></li>
+                <li><Link href="/blog">Blog</Link></li>
+            </ul>
+        </nav>
+    </header>
   )
 }
