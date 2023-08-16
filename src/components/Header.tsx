6538cfeed63fbe08bbec379a1cc43e1a4831b74b
JessicaBane <jessicambane@gmail.com>
2023-08-16 17:08:32 +0100

    added tailwind

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,14 @@
 import Link from "next/link";
+import { merienda, lumanosimo } from "@/font/fonts"
 
 export default function Header() {
   return (
-    <header>
-        <h1>MerTails</h1>
+    <header className={`${merienda.className} p-2`} >
+        <h1 className={`${merienda.className} text-fuchsia-500 text-5xl text-center`}>MerTails</h1>
         <nav>
-            <ul>
-                <li><Link href="/">Home</Link></li>
-                <li><Link href="/blog">Blog</Link></li>
+            <ul className="flex gap-3">
+                <li className=" text-fuchsia-500  hover:text-blue-500"><Link href="/">Home</Link></li>
+                <li className=" text-fuchsia-500 hover:text-blue-500"><Link href="/blog">Blog</Link></li>
             </ul>
         </nav>
     </header>
