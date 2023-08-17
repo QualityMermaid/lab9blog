a54f9724d3d9329a7b1bfd3406757fefb831f40f
JessicaBane <jessicambane@gmail.com>
2023-08-17 09:01:17 +0100

    blur image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,8 @@ export default function Home() {
     <main className=" p-3">
       <h3 className={`${montserrat.className}`}>This is a blog all about Mers! <span className="text-blue-500">Splash</span> in for some <span className="text-purple-500">FIN</span>tastic content!!!</h3>
       <span className=" relative flex justify-center p-3 m-5">
-      <Image src="/images/merssim.png" alt="swimming mers" width={200} height={400} className=" rounded-full p-1 absolute"/>
-      <Image src="/images/merssim.png" alt="swimming mers" width={200} height={400} className=" rounded-full animate-ping-slow p-1 relative"/>
+      <Image src="/images/merssim.png" alt="swimming mers" width={200} height={400} className=" rounded-full p-1 absolute" placeholder="blur"   blurDataURL={`/_next/image?url=/images/merssim.png&w=16&q=1`}/>
+      <Image src="/images/merssim.png" alt="swimming mers" width={200} height={400} className=" rounded-full animate-ping-slow p-1 relative" placeholder="blur"   blurDataURL={`/_next/image?url=/images/merssim.png&w=16&q=1`}/>
       </span>
     </main>
   )
